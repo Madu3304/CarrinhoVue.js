3e47d5b0e0079deb807d536d9d0a0f7c4273cd36
Maria Eduarda Nunes <95004623+Madu3304@users.noreply.github.com>
2024-12-12 17:21:15 -0300

    Add files via upload

---
+++ carrinho/src/components/shared/SlidePanel.vue
@@ -0,0 +1,60 @@
+<script setup>
+//criando propriedades
+defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+
+  visible:{
+    type: Boolean,
+    required: true
+  },
+})
+
+const emit = defineEmits(['update:visible'])
+
+const closePanel = () => {
+  emit('update:visible', false)
+}
+
+</script>
+
+<template>
+  <div class="panel-rap">
+    <div class="panel">
+      <div>
+        <button class="btn error--outline"
+        @click="closePanel()">
+        Close
+      </button>
+      <h2>{{  title }}</h2>
+      </div>
+      <slot name="content"></slot>
+    </div>
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+.panel {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  background-color: aqua;
+  color: azure;
+  overflow: auto;
+  padding: 1em;
+
+  &-wrap {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    background-color: rgb(0, 0, 0, 0.5);
+    z-index: 1;
+  }
+}
+</style>
+
---
+++ carrinho/src/views/HomeView.vue
@@ -0,0 +1,81 @@
+<!-- eslint-disable no-unused-vars -->
+<script setup>
+import { ref } from 'vue';
+import SlidePanel from '../components/shared/SlidePanel.vue';
+
+
+ const cartVisible = ref(true);
+
+ const changeCartVisibility = () => {
+  cartVisible.value = !cartVisible.value;
+  console.log(cartVisible.value);
+ };
+
+</script>
+
+<template>
+  <div class="home">
+    <h1> Shopping Cart</h1>
+    <button class="btn success">Add to Cart</button>
+    <button class="btn error ml-1">Clear Cart</button>
+    <button
+      class="btn error ml-1"
+      @click="changeCartVisibility()">
+      Open Cart</button>
+      <SlidePanel
+          title ="Cart"
+          :visible="cartVisible"
+          @update-visible="changeCartVisibility()">
+      </SlidePanel>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+.btn {
+  &.primry {
+    background-color: aqua;
+    color: bisque;
+
+    &:hover {
+      background-color: #fff;
+      color: #000;
+    }
+  }
+
+  &.success {
+    background-color: rgb(140, 29, 204);
+    color: #000;
+
+    &:hover {
+      background-color: #8ef98e;
+      color: #000;
+    }
+  }
+
+    &.error {
+      background-color: #ff777f;
+      color: #000;
+
+    &:hover {
+      background-color: #df1cae;
+      color: #000;
+    }
+  }
+
+  &--outline {
+    background-color: transparent;
+    color: #000;
+
+    &:hover{
+    background-color: #ff777f;
+    border: #ff777f;
+    color:#fff
+    }
+  }
+}
+
+.ml-1 {
+  margin-left: 3rem;
+}
+</style>
